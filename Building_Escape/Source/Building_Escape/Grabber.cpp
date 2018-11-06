e198b12b9afe86dfd690cc1600f265f7dd539c32
KOTIC <GSOcommando@yahoo.com>
2018-11-05 20:38:35 -0600

    BEL84 LineTraceSingleByObjectType

--- Building_Escape/Source/Building_Escape/Grabber.cpp
+++ Building_Escape/Source/Building_Escape/Grabber.cpp
@@ -62,5 +62,23 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		0.f,
 		10.f
 	);
+
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType
+	(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp,Warning, TEXT("Line trace hit: %s"), *(ActorHit->GetName()))
+	}
 }
 
